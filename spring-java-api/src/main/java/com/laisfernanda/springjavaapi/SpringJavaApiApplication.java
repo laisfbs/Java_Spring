dcfe6f9fca93c37eb7ba8963759bb68cc2ce10c0
Lais <91293868+laisfbs@users.noreply.github.com>
2022-11-17 15:03:19 -0300

    Add files via upload

---
+++ spring-java-api/src/main/java/com/laisfernanda/springjavaapi/SpringJavaApiApplication.java
@@ -0,0 +1,13 @@
+package com.laisfernanda.springjavaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJavaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJavaApiApplication.class, args);
+	}
+
+}
